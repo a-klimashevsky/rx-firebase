c00eb629a540785fd6bc24c80f25a29c550c5bc4
a.klimashevsky <gladiator1988@gmail.com>
2016-08-09 10:49:43 +0300

    initial commit

---
+++ app/src/main/java/com/github/aklimashevsky/rxfirebase/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.aklimashevsky.rxfirebase.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.aklimashevsky.rxfirebase.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
